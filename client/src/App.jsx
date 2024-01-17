31db658e8fa54f2d5bfacbf9476209f776f16de8
nischay-chauhan <masternischay@outlook.com>
2024-01-17 09:26:36 +0530

    intial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="min-h-screen flex justify-center items-center bg-slate-800">
+      <h1 className="text-white text-3xl">Hello world </h1>
+    </div>
+  )
+}
+
+export default App
