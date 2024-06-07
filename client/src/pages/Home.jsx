d03b0b6d8949645d112836b225865362b3c3fc60
nischay-chauhan <masternischay@outlook.com>
2024-06-07 19:35:48 +0530

    Toggle component for enabling the chat

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,6 @@ import Carousal from "../components/Carousal"
 import Categories from "../components/Category"
 import Listings from "../components/Listings"
 import Navbar from "../components/Navbar"
-
 const Home = () => {
   return (
     <div>
