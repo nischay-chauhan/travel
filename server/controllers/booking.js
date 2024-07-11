7b73abbe01d228f3a4425d342f16e3332f1e7509
nischay-chauhan <masternischay@outlook.com>
2024-07-11 16:28:28 +0530

    adding razorpay instance

--- server/controllers/booking.js
+++ server/controllers/booking.js
@@ -1,9 +1,28 @@
 import Listing from "../models/Listing.js";
 import Booking from "../models/Booking.js";
+import Razorpay from "razorpay"
+import dotenv from "dotenv"
+
+dotenv.config();
+
+const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_API_KEY,
+  key_secret: process.env.RAZORPAY_API_SECRET
+})
 
 export const createBooking = async (req, res) => {
   try {
     const { customerId, hostId, listingId, startDate, endDate, totalPrice } = req.body;
+
+    const options = {
+      amount : totalPrice * 100,
+      currency : "INR",
+      receipt : `receipt_order_${Math.random()}`
+    }
+
+    const order = await razorpay.orders.create(options);
+    console.log(order)
+
     const newBooking = new Booking({
       customerId,
       hostId,
@@ -11,6 +30,8 @@ export const createBooking = async (req, res) => {
       startDate,
       endDate,
       totalPrice,
+      paymentStatus: "pending",
+      razorpayOrderId: order.id
     });
     await newBooking.save();
     console.log(hostId)
