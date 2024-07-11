66d2171d97c63196e250ab59749542a768ca55a3
nischay-chauhan <masternischay@outlook.com>
2024-07-11 18:03:56 +0530

    So that only the successfully paid one get sent to frontend

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -4,12 +4,11 @@ import Listing from "../models/Listing.js";
 export const getUserTrips = async (req, res) => {
   try {
     const { userId } = req.params;
-    const trips = await Booking.find({ customerId: userId })
+    const trips = await Booking.find({ customerId: userId, paymentStatus: "Paid" })
       .populate("customerId hostId listingId")
       .exec();
 
     res.status(200).json(trips);
-    // console.log(trips)
   } catch (error) {
     console.error("Error fetching user trips:", error);
     res.status(500).json({ message: "Failed to get user trips", error: error.message });
